f36b778bf45b8d0bf6815b00125460626ca1d684
Foranser <dforanser@gmail.com>
2026-01-15 20:49:56 +0700

    Add TypeModel reflection cache

---
+++ Task1/TypeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task1;
+
+public sealed class TypeModel
+{
+    public FieldInfo[] Fields { get; }
+
+    public TypeModel(Type type, string[] fieldOrder)
+    {
+        var flags = BindingFlags.Instance | BindingFlags.NonPublic;
+        var fields = new FieldInfo[fieldOrder.Length];
+
+        for (int i = 0; i < fieldOrder.Length; i++)
+        {
+            var f = type.GetField(fieldOrder[i], flags);
+            if (f == null)
+                throw new InvalidOperationException($"Field not found: {fieldOrder[i]}");
+            fields[i] = f;
+        }
+
+        Fields = fields;
+    }
+}
