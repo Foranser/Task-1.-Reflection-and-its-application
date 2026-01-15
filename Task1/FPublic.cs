460df46bc5d53c2bfd8d99403a42e76edeaa1edf
Foranser <dforanser@gmail.com>
2026-01-15 20:47:23 +0700

    Add FPublic DTO

---
+++ Task1/FPublic.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1;
+
+public sealed class FPublic
+{
+    public int I1 { get; set; }
+    public int I2 { get; set; }
+    public int I3 { get; set; }
+    public int I4 { get; set; }
+    public int I5 { get; set; }
+    public int[] Mas { get; set; } = [];
+}
